81284fd60fccb97118e58cb7bf17b7b2465047dd
Mithun-MP Dev <162953538+MITHUNMP157@users.noreply.github.com>
2025-10-04 18:30:08 +0530

    HashRouter removed
    HashRouter removed

--- src/index.js
+++ src/index.js
@@ -9,11 +9,8 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-
     <Provider store={store}>
-  <HashRouter>
       <App />
-  </HashRouter>
     </Provider>
   </React.StrictMode>
 );
